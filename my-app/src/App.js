2d3950822d0ff66acd3e8775d724525028ef64d4
esmabecovic <becovicesma@gmail.com>
2023-07-14 16:36:02 +0200

    header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
 import CoronaInfoFunc from './Components/coronaInfoCards/coronaInfo';
+import Header from './pages/header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <CoronaInfoFunc/>
     </div>
   );
