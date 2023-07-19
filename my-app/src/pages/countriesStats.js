302d9c918c9df621c01e185d14ad6d5942dd724a
zeindelic@gmail.com <zeindelic@gmail.com>
2023-07-19 07:05:55 -0700

    country stats done

--- my-app/src/pages/countriesStats.js
+++ my-app/src/pages/countriesStats.js
@@ -1,4 +1,56 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 
+export default function CountrySelect({ countryNameFunc }) {
+  return (
+    <Autocomplete
+      onChange={(event, value) => {
+        countryNameFunc(value.searchName, value.code.toLowerCase());
+      }}
+      defaultValue={countries.find((el) => el.label === "Serbia")}
+      id="country-select-demo"
+      sx={{
+        width: 300,
+      }}
+      options={countries}
+      autoHighlight
+      getOptionLabel={(option) => option.label}
+      renderOption={(props, option) => (
+        <Box
+          component="li"
+          sx={{
+            "& > img": { mr: 2, flexShrink: 0 },
+          }}
+          {...props}
+        >
+          <img
+            loading="lazy"
+            width="20"
+            src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
+            srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
+            alt=""
+          />
+          {option.label} ({option.code})
+        </Box>
+      )}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Choose a country"
+          inputProps={{
+            ...params.inputProps,
+            autoComplete: "new-password", // disable autocomplete and autofill
+          }}
+          sx={{
+            backgroundColor: "white",
+          }}
+        />
+      )}
+    />
+  );
+}
 
 
     
--- my-app/src/pages/country_stats.js
+++ my-app/src/pages/country_stats.js
@@ -1,99 +1,154 @@
-import React, { useState } from "react";
+import React from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import {
-  CountryFlagCard,
-  CountryInfoPart,
-  CountryStatsCard,
-  CountryStatsMain,
-} from "../Components/styleComponents/index.style";
- 
-const CountryStats = () => {
-  const [searchValue, setSearchValue] = useState("");
-  const [countryData, setCountryData] = useState(null);
-  const [errorMessage, setErrorMessage] = useState("");
- 
-  const fetchCountryStats = async () => {
-    try {
-      const options = {
-        method: "GET",
-        url: "https://covid-193.p.rapidapi.com/statistics",
-        headers: {
-          "X-RapidAPI-Key": "cc28d2e5d1msh517cb34697fab94p106bddjsnb52786ff6b23",
-          "X-RapidAPI-Host": "covid-193.p.rapidapi.com",
-        },
-      };
- 
-      const response = await axios.request(options);
-      const countryStats = response.data.response.find(
-        (country) => country.country.toLowerCase() === searchValue.toLowerCase()
-      );
- 
-      if (countryStats) {
-        setCountryData(countryStats);
-        setErrorMessage("");
-      } else {
-        setCountryData(null);
-        setErrorMessage("Country not found");
-      }
-    } catch (error) {
-      console.error(error);
-      setCountryData(null);
-      setErrorMessage("An error occurred while fetching data");
-    }
+import CountrySelect from "./countriesStats";
+import MaterialUIPickers from "../Components/pickDate/pickDate";
+import StateCard from "./StateCard";
+
+//get Date
+const justDay =
+  new Date().getDate() >= 10
+    ? new Date().getDate()
+    : "0".concat(new Date().getDate());
+
+const justMonth =
+  new Date().getMonth() >= 10
+    ? new Date().getMonth()
+    : new Date().getMonth() === 9
+    ? "10"
+    : "0".concat(new Date().getMonth() + 1);
+const onThisDay = new Date().getFullYear() + "-" + justMonth + "-" + justDay;
+
+//component
+function CountryStats() {
+  const [countryName, setCountryName] = useState({
+    name: "Serbia",
+    imgUrl: "rs",
+    date: `${onThisDay}`,
+  });
+  const [countryData, setCountryData] = useState({
+    new_cases: "",
+    new_death: "",
+    new_recovered: "",
+    total_active: "",
+  });
+
+  const options = {
+    method: "GET",
+    url: "https://covid-193.p.rapidapi.com/history",
+    timeout: 60000,
+    params: { country: countryName.name, day: countryName.date },
+
+    headers: {
+      "X-RapidAPI-Key": "e0d331ecf0msh511ca18a17a79e5p1481dfjsna27313c7f6c9",
+      "X-RapidAPI-Host": "covid-193.p.rapidapi.com",
+    },
+    clarifyTimeoutError: false,
   };
- 
-  const handleChange = (event) => {
-    setSearchValue(event.target.value);
-  };
- 
-  const handleSearch = (event) => {
-    event.preventDefault();
-    if (searchValue.trim() !== "") {
-      fetchCountryStats();
-    }
-  };
- 
+
+  useEffect(() => {
+    axios
+      .request(options, { date: countryName.date })
+      .then(function (response) {
+        const seconArr = response.data.response?.reduce(
+          (acc, curr) => {
+            return {
+              ...acc,
+              new_cases: curr.cases.new ? curr.cases.new : "/",
+              new_death: curr.deaths.new ? curr.deaths.new : "/",
+              new_recovered: curr.cases.recovered
+                ? "+" + curr.cases.recovered
+                : "/",
+              total_active: curr.cases.active ? curr.cases.active : "/",
+            };
+          },
+          {
+            new_cases: "",
+            new_death: "",
+            new_recovered: "",
+            total_active: "",
+          }
+        );
+
+        setCountryData({
+          new_cases: "",
+          new_death: "",
+          new_recovered: "",
+          total_active: "",
+        });
+
+        response.data.response.length
+          ? setCountryData(seconArr)
+          : setCountryData({
+              new_cases: "no-data",
+              new_death: "no-data",
+              new_recovered: "no-data",
+              total_active: "no-data",
+            });
+      })
+      .catch(function (error) {
+        console.error(error);
+        setCountryData({
+          new_cases: "no-data",
+          new_death: "no-data",
+          new_recovered: "no-data",
+          total_active: "no-data",
+        });
+      });
+    window.scrollTo(0, 0);
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [countryName.name, countryName.date]);
   return (
-    <CountryStatsMain>
-      <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          placeholder="Enter a country..."
-          value={searchValue}
-          onChange={handleChange}
+    <div
+      className="countryStatsMainDiv"
+      style={{
+        backgroundImage:
+          "url(https://northernvirginiamag.com/wp-content/uploads/2020/06/people-with-masks.jpg)",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="flex justify-center gap-2">
+        <CountrySelect
+          countryNameFunc={(nameOfState, imgOfState) =>
+            setCountryName((prev) => {
+              setCountryData({
+                new_cases: "",
+                new_death: "",
+                new_recovered: "",
+                total_active: "",
+              });
+              return { ...prev, name: nameOfState, imgUrl: imgOfState };
+            })
+          }
         />
-        <button type="submit">Search</button>
-      </form>
-      {errorMessage && <div>{errorMessage}</div>}
-      {countryData && (
-        <CountryStatsCard>
-          <CountryInfoPart>
-            <h1 className="countryName">Country: {countryData.country}</h1>
-            <ul className="casesInfo">
-              <li>
-                Population: <span className="population">{countryData.population}</span>
-              </li>
-              <li>
-                New Cases: <span className="newCases">{countryData.cases.new}</span>
-              </li>
-              <li>
-                Deaths: <span className="deaths">{countryData.deaths.new}</span>
-              </li>
-              <li>
-                Recovered: <span className="recovered">{countryData.cases.recovered}</span>
-              </li>
-              <li>
-                Total Active: <span className="totalActive">{countryData.cases.active}</span>
-              </li>
-            </ul>
-          </CountryInfoPart>
-          <CountryFlagCard>
-            //<img Ne mogu nikako da napravim da se slika prikazuje ako budes znao prepravi pls />
-          </CountryFlagCard>
-        </CountryStatsCard>
-      )}
-    </CountryStatsMain>
+        <MaterialUIPickers
+          dateFunc={(val) => {
+            setCountryData({
+              new_cases: "",
+              new_death: "",
+              new_recovered: "",
+              total_active: "",
+            });
+            setCountryName((prev) => {
+              return { ...prev, date: val };
+            });
+          }}
+        />
+      </div>
+      <div>
+        <StateCard
+          countryName={countryName?.name}
+          countryImg={countryName?.imgUrl}
+          new_cases={countryData.new_cases}
+          new_death={countryData?.new_death}
+          new_recovered={countryData?.new_recovered}
+          total_active={countryData?.total_active}
+        />
+      </div>
+    </div>
   );
-};
- 
+}
+
 export default CountryStats;
